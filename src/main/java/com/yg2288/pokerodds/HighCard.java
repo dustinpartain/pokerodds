c4f6d8099e0877d07379c23409030f99df8cbab7
Alex Ge <alex.ge@sas.com>
2019-12-30 16:39:15 -0600

    Implement HighCard

---
+++ src/main/java/com/yg2288/pokerodds/HighCard.java
@@ -0,0 +1,18 @@
+package com.yg2288.pokerodds;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HighCard extends PlayingHand {
+    public HighCard(List<Card> cards) {
+        super(cards);
+    }
+
+    public Card highCard() {
+        return Collections.max(cards);
+    }
+
+    public Card lowCard() {
+        return Collections.min(cards);
+    }
+}
