32a09380ad9e8fa64d7a4b17ebec6ac110303b98
Alex Ge <yg2288@columbia.edu>
2020-01-02 21:04:25 -0500

    Adding getters for rank in ThreeOfAKind

--- src/main/java/com/yg2288/pokerodds/ThreeOfAKind.java
+++ src/main/java/com/yg2288/pokerodds/ThreeOfAKind.java
@@ -1,27 +1,37 @@
 package com.yg2288.pokerodds;
 
+import java.util.Collections;
 import java.util.List;
 
 public class ThreeOfAKind extends PlayingHand {
-    List<Card> three;
-    List<Card> rest;
+    private List<Card> triplet;
+    private List<Card> rest;
 
-    protected boolean isThree(List<Card> three) {
-        if (three.size() != 3)
+    protected boolean isThree(List<Card> triplet) {
+        if (triplet.size() != 3)
             return false;
-        if (three.get(0).getRank() != three.get(1).getRank() ||
-            three.get(1).getRank() != three.get(2).getRank())
+        if (triplet.get(0).getRank() != triplet.get(1).getRank() ||
+                triplet.get(1).getRank() != triplet.get(2).getRank())
             return false;
         return true;
     }
 
-    public ThreeOfAKind(List<Card> three, List<Card> rest) {
-        super(three);
-        if (!isThree(three))
+    public ThreeOfAKind(List<Card> triplet, List<Card> rest) {
+        super(triplet);
+        if (!isThree(triplet))
             throw new IllegalArgumentException("Not three of a kind. ");
         if (rest.size() != 2)
             throw new IllegalArgumentException("Hand must be 5 cards. ");
         for (Card c : rest)
             cards.add(c.clone());
+        Collections.sort(rest);
+    }
+
+    public List<Card> getTriplet() {
+        return this.triplet;
+    }
+
+    protected Card.Rank getTripletRank() {
+        return this.triplet.get(0).getRank();
     }
 }
