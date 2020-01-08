5a0d89c35ee7997bf3fb29cf5e8e2901cac45e71
Alex Ge <alex.ge@sas.com>
2020-01-07 20:36:04 -0500

    Add FourOfAKind

---
+++ src/main/java/com/yg2288/pokerodds/FourOfAKind.java
@@ -0,0 +1,55 @@
+package com.yg2288.pokerodds;
+
+import static com.yg2288.pokerodds.Card.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class FourOfAKind extends PlayingHand {
+    public static final HandEnum type = HandEnum.FOUROFAKIND;
+
+    public static boolean isFourOfAKind(List<Card> cards) {
+        if (cards.size() != 5)
+            return false;
+        int[] count = new int[13];
+        for (Card c : cards)
+            count[c.getRank().ordinal()]++;
+        for (int i : count)
+            if (i == 4)
+                return true;
+        return false;
+    }
+
+    protected List<Card> quad;
+    protected Card kicker;
+
+    public FourOfAKind(List<Card> cards) {
+        super(cards);
+        if (!isFourOfAKind(cards))
+            throw new IllegalArgumentException("Not four of a kind. ");
+        quad = findSame(this.cards, 4);
+        kicker = findSame(this.cards, 1).get(0);
+    }
+
+    protected List<Card> findSame(List<Card> cards, int count) {
+        List<List<Card>> l = new ArrayList<>();
+        for (int i=0; i<13; i++) {
+            l.add(new ArrayList<>());
+        }
+        for (Card c : cards)
+            l.get(c.getRank().ordinal()).add(c);
+        for (List<Card> c : l)
+            if (c.size() == count)
+                return c;
+        return null;
+    }
+
+    protected Rank getQuadRank() {
+        return quad.get(0).getRank();
+    }
+
+    protected Rank getKickerRank() {
+        return kicker.getRank();
+    }
+}
---
+++ src/test/java/com/yg2288/pokerodds/FourOfAKindTest.java
@@ -0,0 +1,58 @@
+package com.yg2288.pokerodds;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static com.yg2288.pokerodds.Card.*;
+
+public class FourOfAKindTest {
+    protected List<Card> getCards1() {
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card(Suit.CLUBS, Rank.THREE));
+        cards.add(new Card(Suit.DIAMONDS, Rank.THREE));
+        cards.add(new Card(Suit.HEARTS, Rank.THREE));
+        cards.add(new Card(Suit.SPADES, Rank.THREE));
+        cards.add(new Card(Suit.CLUBS, Rank.TWO));
+        return cards;
+    }
+
+    protected FourOfAKind getHand1() {
+        return new FourOfAKind(getCards1());
+    }
+
+    protected FourOfAKind getHand2() {
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card(Suit.CLUBS, Rank.FOUR));
+        cards.add(new Card(Suit.DIAMONDS, Rank.FOUR));
+        cards.add(new Card(Suit.HEARTS, Rank.FOUR));
+        cards.add(new Card(Suit.SPADES, Rank.FOUR));
+        cards.add(new Card(Suit.CLUBS, Rank.ACE));
+        return new FourOfAKind(cards);
+    }
+
+    protected FourOfAKind getHand3() {
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card(Suit.CLUBS, Rank.FOUR));
+        cards.add(new Card(Suit.DIAMONDS, Rank.FOUR));
+        cards.add(new Card(Suit.HEARTS, Rank.FOUR));
+        cards.add(new Card(Suit.SPADES, Rank.FOUR));
+        cards.add(new Card(Suit.CLUBS, Rank.THREE));
+        return new FourOfAKind(cards);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fourKindTest1() {
+        List<Card> cards = getCards1();
+        cards.get(0).setRank(Rank.ACE);
+        new FourOfAKind(cards);
+    }
+
+    @Test
+    public void fourKindTest2() {
+        FourOfAKind hand1 = getHand1();
+        assertEquals(Rank.THREE, hand1.getQuadRank());
+        assertEquals(Rank.TWO, hand1.getKickerRank());
+    }
+}
