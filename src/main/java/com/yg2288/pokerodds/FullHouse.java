14ce0d9a08c9fa1c77ae4920f3015501a2ec30ae
Alex Ge <alex.ge@sas.com>
2020-01-12 22:04:28 -0500

    Update FullHouse

--- src/main/java/com/yg2288/pokerodds/FullHouse.java
+++ src/main/java/com/yg2288/pokerodds/FullHouse.java
@@ -11,47 +11,26 @@ public class FullHouse extends PlayingHand implements Comparable<FullHouse> {
     private List<Card> triplet;
     private List<Card> pair;
 
-    public static boolean isFullHouse(List<Card> cards) {
+    public static boolean isValid(List<Card> cards) {
         if (cards.size() != 5)
             return false;
-        int[] count = new int[15];
-        for (Card c: cards) {
-            count[c.getRank().ordinal()]++;
-        }
-        boolean hasTriplet = false;
-        boolean hasPair = false;
-        for (int i : count) {
-            if (i == 2)
-                hasPair = true;
-            if (i == 3)
-                hasTriplet = true;
-        }
-        return hasPair && hasTriplet;
+        int[] profile = {11, 0, 1, 1, 0};
+        if (!Arrays.equals(profile, PlayingHand.getProfile(cards)))
+            return false;
+        return true;
     }
 
     public FullHouse(List<Card> cards) {
         super(cards);
-        if (!isFullHouse(cards))
+        if (!isValid(this.cards))
             throw new IllegalArgumentException("Not a full house. ");
-        this.triplet = findPair(this.cards, 3);
-        this.pair = findPair(this.cards, 2);
-    }
-
-    protected List<Card> findPair(List<Card> cards, int count) {
-        List<List<Card>> triplet = new ArrayList<>();
-        for (int i=0; i<15; i++)
-            triplet.add(new ArrayList<>());
-        for (int i=0; i<cards.size(); i++) {
-            Card c = cards.get(i);
-            if (triplet.get(c.getRank().ordinal()) == null)
-                triplet.set(c.getRank().ordinal(), new ArrayList<>());
-            triplet.get(c.getRank().ordinal()).add(c);
+        List<List<Card>> bucket = PlayingHand.getBuckets(this.cards);
+        for (List<Card> l : bucket) {
+            if (l.size() == 3)
+                this.triplet = l;
+            if (l.size() == 2)
+                this.pair = l;
         }
-        for (List<Card> cardList : triplet) {
-            if (cardList.size() == count)
-                return cardList;
-        }
-        return null;
     }
 
     public Card.Rank getTripletRank() {
