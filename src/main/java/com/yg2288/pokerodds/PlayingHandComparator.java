d549a423f729d9dd8b34b2e3e04ab1c16af7aecf
Alex Ge <alex.ge@sas.com>
2020-01-19 19:42:52 -0500

    Add PlayingHandComparator

---
+++ src/main/java/com/yg2288/pokerodds/PlayingHandComparator.java
@@ -0,0 +1,9 @@
+package com.yg2288.pokerodds;
+
+import java.util.Comparator;
+
+public class PlayingHandComparator implements Comparator<PlayingHand> {
+    public int compare(PlayingHand one, PlayingHand two) {
+        return 0;
+    }
+}
