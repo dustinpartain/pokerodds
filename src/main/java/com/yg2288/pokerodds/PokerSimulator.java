471c4774ca4b78504a27105b676afd640ecefbe5
Alex Ge <alex.ge@sas.com>
2020-01-22 15:55:22 -0500

    Add getters and setters for PokerSimulator

--- src/main/java/com/yg2288/pokerodds/PokerSimulator.java
+++ src/main/java/com/yg2288/pokerodds/PokerSimulator.java
@@ -3,11 +3,53 @@ package com.yg2288.pokerodds;
 import java.util.List;
 
 public class PokerSimulator {
-    StartingHand playerHand;
-    List<StartingHand> opponentHands;
+    private StartingHand playerHand;
+    private List<StartingHand> opponentHands;
+    private List<Card> board;
 
-    public PokerSimulator(StartingHand playerHand, List<StartingHand> opponentHands) {
+    public PokerSimulator(StartingHand playerHand, List<StartingHand> opponentHands, List<Card> board) {
         this.playerHand = playerHand;
         this.opponentHands = opponentHands;
+        this.board = board;
+    }
+
+    public List<StartingHand> getOpponentHands() {
+        return opponentHands;
+    }
+
+    public void setOpponentHands(List<StartingHand> opponentHands) {
+        this.opponentHands = opponentHands;
+    }
+
+    public void setOpponentHands(int i, StartingHand opponentHand) {
+        if (i < 0 || i >= opponentHands.size())
+            throw new IllegalArgumentException("Invalid opponent number. ");
+        opponentHands.set(i, opponentHand);
+    }
+
+    public int getNumOpponents() {
+        return opponentHands.size();
+    }
+
+    public StartingHand getPlayerHand() {
+        return playerHand;
+    }
+
+    public void setPlayerHand(StartingHand playerHand) {
+        this.playerHand = playerHand;
+    }
+
+    public List<Card> getBoard() {
+        return board;
+    }
+
+    public void setBoard(List<Card> board) {
+        this.board = board;
+    }
+
+    public void setBoard(int i, Card card) {
+        if (i < 0 || i >= board.size())
+            throw new IllegalArgumentException("Invalid card number. ");
+        board.set(i, card);
     }
 }
