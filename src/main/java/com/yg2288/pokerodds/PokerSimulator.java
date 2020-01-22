e9f7eeec726424940378b126565af5b652bb9e9a
Alex Ge <alex.ge@sas.com>
2020-01-22 16:13:16 -0500

    Add stats for PokerSimulator

--- src/main/java/com/yg2288/pokerodds/PokerSimulator.java
+++ src/main/java/com/yg2288/pokerodds/PokerSimulator.java
@@ -1,11 +1,16 @@
 package com.yg2288.pokerodds;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class PokerSimulator {
     private StartingHand playerHand;
     private List<StartingHand> opponentHands;
     private List<Card> board;
+
+    private int gamesPlayed;
+    private int gamesWon;
+    private HashMap<HandEnum, Integer> playerHandStats;
 
     public PokerSimulator(StartingHand playerHand, List<StartingHand> opponentHands, List<Card> board) {
         this.playerHand = playerHand;
@@ -51,5 +56,30 @@ public class PokerSimulator {
         if (i < 0 || i >= board.size())
             throw new IllegalArgumentException("Invalid card number. ");
         board.set(i, card);
+    }
+
+    public void resetStats() {
+        gamesPlayed = gamesWon = 0;
+        playerHandStats.clear();
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public float getWinPercentage() {
+        return (float)gamesWon / gamesPlayed;
+    }
+
+    public HashMap<HandEnum, Integer> getHandStats() {
+        return playerHandStats;
+    }
+
+    public int getHandStat(HandEnum type) {
+        return playerHandStats.getOrDefault(type, 0);
     }
 }
